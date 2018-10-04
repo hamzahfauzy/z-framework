9104d3d30b6d359f8f95d8ccfe8cf70abb05869b
hamzahfauzy <hamzahfauzy97@gmail.com>
2018-10-04 12:15:04 +0700

    base code

--- config/Path.php
+++ config/Path.php
@@ -1,3 +1,3 @@
 <?php
 
-define("path_name","z-framework");
+define("path_name","");
--- config/Routes.php
+++ config/Routes.php
@@ -3,15 +3,3 @@ use vendor\zframework\Route;
 
 Route::get("/","IndexController@index");
 
-Route::prefix("/admin")->middleware("Admin")->namespace("Admin")->group(function(){
-	Route::get("/","IndexController@index");
-});
-
-Route::get("/hello","IndexController@hello");
-Route::get("/tryparam/{param}","IndexController@tryparam");
-Route::get("/user/{id}","IndexController@user");
-Route::post("/posthandle","IndexController@posthandle");
-Route::get("/login","IndexController@login");
-Route::get("/register","IndexController@register");
-Route::get("/logout","IndexController@logout");
-
